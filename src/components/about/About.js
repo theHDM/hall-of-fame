384fbbed9407a1cca0fb3a43375d2a6c7e5c3a0b
codenotfound <dmitry.velikij@gmail.com>
2018-04-15 23:12:49 +0300

    about page update

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const About = () => (
     <div className="container">
-        <div class="jumbotron mt-2 bg-dark text-white">
-            <h1 class="display-3 text-white">About</h1>
-            <p class="lead text-white">
+        <div className="jumbotron mt-2 bg-dark text-white">
+            <h1 className="display-3 text-white">About</h1>
+            <p className="lead text-white">
                 Carnage Quake League is an esports organization for non-pro players.
                 We host Uniorz Challenge cups in Quake Champions and Quake Live.
                 We also have an active Discord community.
             </p>
-            <hr class="my-4 bg-light"/>
+            <hr className="my-4 bg-light"/>
                 <h4 className="text-white">Social links:</h4>
             <ul>
                 <li><a href="https://discord.gg/XRKU4py" className="text-white">Discord</a></li>
